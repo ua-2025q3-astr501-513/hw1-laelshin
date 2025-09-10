2cba97ec48334cf5582529006986210548af9b0c
Lael Shin <147151153+laelshin@users.noreply.github.com>
2025-09-09 18:00:43 -0700

    Update p3.py

--- hw1/p3.py
+++ hw1/p3.py
@@ -30,6 +30,7 @@
 # Here, let's implement a numerically stable quadratic equation solver
 # to overcome catastrophic cancellation.
 # Please make sure that you take care of all the special cases.
+import numpy as np
 
 def quadratic(a, b, c):
     """Numerically stable quadratic equation solver
@@ -65,3 +66,30 @@ def quadratic(a, b, c):
                 If there is no real root, x1 == x2 == None.
     """
     # TODO: implement the stable quadratic equation solver here
+    # We should avoid the direct subtraction like b-b
+    # x = (-b +-sqrt(b**2 - 4*a*c))/(2a) -> multiply 1 = (-b -+sqrt(b**2 - 4*a*c)) / (-b -+sqrt(b**2 - 4*a*c))
+    # then, x = (2c) / (-b -+sqrt(b**2 - 4*a*c))
+    # we can avoid the b - b calculation by selecting appropriate sign
+    
+    disc = b*b - 4*a*c
+    
+    if disc < 0:
+        x1 = None
+        x2 = None
+        return x1, x2
+
+    if b >= 0:
+        x1 = (-1*b - np.sqrt(disc))/(2*a)
+        x2 = (2*c)/(-1*b - np.sqrt(disc))
+
+    elif b < 0:
+        x1 = (2*c)/(-1*b + np.sqrt(disc))
+        x2 = (-1*b + np.sqrt(disc))/(2*a)
+
+    if np.isclose(x1, x2): # check whether x1 == x2
+        x2 = None
+
+    if x2 is not None and x1 > x2:
+        x1, x2 = x2, x1
+        
+    return x1, x2
